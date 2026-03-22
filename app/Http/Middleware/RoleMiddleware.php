48af3af12c591fe3617c9a22483ec1cb2b85e617
Jonathan Peguit Jr. <jonathanjrhayo@gmail.com>
2026-03-22 10:46:23 +0800

    feat(middleware): add RoleMiddleware to handle user role authorization

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string ...$roles): Response
+    {
+
+        if(!$request->user()){
+            return redirect('login');
+        }
+
+        // check user role
+
+        if(!$request->user()->hasAnyRole($roles)){
+            abort(403, 'Unauthorized. Insufficient permissions.');
+        }
+        return $next($request);
+    }
+}
